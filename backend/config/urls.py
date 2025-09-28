c9b539621bd8d3959cdfde7e5da760966f6cefce
Nicola Cantalupo <nicolacantalupo@libero.it>
2025-09-28 18:53:05 +0200

    loginout

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,3 +1,4 @@
+
 from django.contrib import admin
 from django.urls import path, include
 from .views import home
@@ -5,5 +6,6 @@ from .views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('prenotazioni.urls')),  # file prenotazioni/urls.py
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', home, name='home'),
 ]
