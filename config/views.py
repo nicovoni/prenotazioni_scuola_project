91238a3dfd3345ea34b933684ff17133c3a5bcfe
Nicola Cantalupo <nicolacantalupo@libero.it>
2025-11-16 21:40:21 +0100

    reversqas

--- config/views.py
+++ config/views.py
@@ -6,7 +6,7 @@ def home(request):
     # Controllo se il sistema è configurato
     from prenotazioni.models import Utente, Risorsa
     if not Utente.objects.exists() or not Risorsa.objects.exists():
-        return redirect('configurazione_sistema')
+        return redirect('prenotazioni:configurazione_sistema')
     return render(request, 'home.html')
 
 def health_check(request):
