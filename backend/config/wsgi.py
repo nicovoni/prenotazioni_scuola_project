60e48a395395afbc9916e7c067284f9b582f654c
Nicola Cantalupo <nicolacantalupo@libero.it>
2025-11-16 15:10:01 +0100

    bbb

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,9 +1,5 @@
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
-
-# Aggiungi il path del progetto per trovare i moduli
-sys.path.insert(0, '/app')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.config.settings')
 application = get_wsgi_application()
