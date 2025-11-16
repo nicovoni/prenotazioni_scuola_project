ef553bfccf60737aba9637a1aa0e31b5d1b5f420
Nicola Cantalupo <nicolacantalupo@libero.it>
2025-11-16 16:23:22 +0100

    ssss

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+# La variabile DJANGO_SETTINGS_MODULE viene impostata nel render.yaml
 application = get_wsgi_application()
