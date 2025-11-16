d0fc96bd2e65e85e7cd32b8ca903a5854a217a74
Nicola Cantalupo <nicolacantalupo@libero.it>
2025-11-16 14:54:43 +0100

    sundeq

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,5 +1,9 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Aggiungi il path del progetto per trovare i moduli
+sys.path.insert(0, '/app')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.config.settings')
 application = get_wsgi_application()
