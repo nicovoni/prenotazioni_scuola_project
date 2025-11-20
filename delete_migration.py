2d675fb4039b51687d73cc09d80a97b998797215
nicola <nicolacantalupo@libero.it>
2025-11-20 09:13:54 +0100

    f

--- delete_migration.py
+++ delete_migration.py
@@ -7,5 +7,10 @@ django.setup()
 from django.db import connection
 
 with connection.cursor() as cursor:
+    # Delete the migration record if it exists
     cursor.execute("DELETE FROM django_migrations WHERE app = 'prenotazioni' AND name = '0001_initial'")
     print("Deleted prenotazioni migration record")
+
+    # Clean up any content types with null name to fix IntegrityError
+    cursor.execute("DELETE FROM django_content_type WHERE name IS NULL")
+    print("Cleaned up bad content type records")
